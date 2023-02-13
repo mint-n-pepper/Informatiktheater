2ec1da4ca7be0a9aea85d1f0b8a987ac2ec68ccf
sja <silvan.januth@artorg.unibe.ch>
2023-02-13 16:13:02 +0100

    removed callback functions

--- StartbitV2.ts
+++ StartbitV2.ts
@@ -906,32 +906,32 @@ namespace Informatiktheater {
     pins.setPull(pin, PinPullMode.PullUp);
   }
 
-  //% weight=1
-  //% block="Trittmatte pressed"
-  //% block.loc.de="Trittmatte gedrückt"
-  //% subcategory=Trittmatte
-  export function trittmatte_pressed(handler: () => void) {
-    console.log("trittmatte gedrückt");
-    handler();
-  }
-
-  /**
-   * Registers Trittmatte event handler.
-   */
-  //% weight=20 blockGap=8 blockId="control_on_event_trittmatte"
-  //%block="on event|from %src=control_event_source_id|with value %value=control_event_value_id"
-  //% blockExternalInputs=1
-  //% subcategory=Trittmatte
-  export function onEventTrittmatte(
-    src: number,
-    value: number,
-    handler: () => void,
-    flags: number = 0
-  ) {
-    if (!flags) flags = EventFlags.QueueIfBusy;
-    pxtcore.registerWithDal(src, value, handler, flags);
-  }
-
+  // //% weight=1
+  // //% block="Trittmatte pressed"
+  // //% block.loc.de="Trittmatte gedrückt"
+  // //% subcategory=Trittmatte
+  // export function trittmatte_pressed(handler: () => void) {
+  //   console.log("trittmatte gedrückt");
+  //   handler();
+  // }
+  //
+  // /**
+  //  * Registers Trittmatte event handler.
+  //  */
+  // //% weight=20 blockGap=8 blockId="control_on_event_trittmatte"
+  // //%block="on event|from %src=control_event_source_id|with value %value=control_event_value_id"
+  // //% blockExternalInputs=1
+  // //% subcategory=Trittmatte
+  // export function onEventTrittmatte(
+  //   src: number,
+  //   value: number,
+  //   handler: () => void,
+  //   flags: number = 0
+  // ) {
+  //   if (!flags) flags = EventFlags.QueueIfBusy;
+  //   pxtcore.registerWithDal(src, value, handler, flags);
+  // }
+  //
   // MP3 Player stuff
 
   export class SongList {
