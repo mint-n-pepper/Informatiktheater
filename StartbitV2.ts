c60c0f6a7bf5bfb40ecc4a5f4e08945de621ad02
Zauberhut <49160827+Zauberhut@users.noreply.github.com>
2023-03-13 08:42:31 +0100

    Update StartbitV2.ts
    added //% angle.shadow="protractorPicker"

--- StartbitV2.ts
+++ StartbitV2.ts
@@ -321,6 +321,7 @@ namespace Informatiktheater {
   //% blockId=setServo
   //% block="set servo motor %index| angle (°) %angle| duration (ms) %duration"
   //% block.loc.de="setze Servomotor %index| auf Winkel (0 - 180°) %angle|für Dauer (ms) %duration"
+  //% angle.shadow="protractorPicker"
   //% angle.min=0 angle.max=180
   //% index.defl=1
   //% duration.shadow=timePicker
