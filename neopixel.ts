4a3213facd0d9e86fdde277eb64d88b2960c3389
sja <silvan.januth@artorg.unibe.ch>
2023-03-14 15:03:30 +0100

    keep direction alway clockwise

--- neopixel.ts
+++ neopixel.ts
@@ -96,12 +96,12 @@ namespace neopixel {
             const saturation = 100;
             const luminance = 50;
             const steps = this._length;
-            let direction;
-            if (endHue > startHue) {
-                direction = HueInterpolationDirection.Clockwise;
-            } else if (endHue < startHue) {
-                direction = HueInterpolationDirection.CounterClockwise;
-            }
+            const direction = HueInterpolationDirection.Clockwise;
+            // if (endHue > startHue) {
+            //     direction = HueInterpolationDirection.Clockwise;
+            // } else if (endHue < startHue) {
+            //     direction = HueInterpolationDirection.CounterClockwise;
+            // }
 
             //hue
             const h1 = startHue;
